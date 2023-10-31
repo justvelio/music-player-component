cc6aeb792588cf84fcb3ea181f6e4b08b637fefb
Velislav Kostadinov <velislav.kostadinov222@gmail.com>
2023-10-31 20:42:53 +0200

    prevent video pop-out bug;

--- music-player/src/components/VideoPlayer.jsx
+++ music-player/src/components/VideoPlayer.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useRef } from 'react';
 import './VideoPlayer.css';
 
 const VideoPlayer = ({ videoSrc }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.controls = false;
+    }
+  }, []);
+  
     return (
         <video autoPlay loop muted playsInline ref={(ref) => { if (ref) ref.playbackRate = 0.8; }} className="video-container">
           <source src={videoSrc} type="video/mp4" />
