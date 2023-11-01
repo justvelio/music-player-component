e90d587ec26ae1ff3eb8ad888643f23ab5ae1a25
Velislav Kostadinov <velislav.kostadinov222@gmail.com>
2023-11-01 13:06:35 +0200

    fix app.jsx

--- music-player/src/App.jsx
+++ music-player/src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import AudioPlayer from "./components/AudioPlayer";
 import VideoPlayer from "./components/VideoPlayer";
-import audioFile from './audio/audio.mp3';
-import videoFile from './video/video.mp4';
+import audioFile from "./audio/audio.mp3";
+import videoFile from "./video/video.mp4";
 
 function App() {
   return (
-    <div className="container">
-      <VideoPlayer videoSrc={videoFile} />
-      <div className="content">
-        <AudioPlayer audioSrc={audioFile} />
+    <>
+      <div className="container">
+        <VideoPlayer videoSrc={videoFile} />
+        <div className="content">
+          <AudioPlayer audioSrc={audioFile} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
